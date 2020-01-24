6ded86b4dd42da372c8fde5f265cd3112c0ca565
Alessandro Varuzza <alessandrovaru@gmail.com>
2020-01-24 11:03:44 -0400

    Get data from API

--- js/index.js
+++ js/index.js
@@ -7,18 +7,16 @@ let resultado = document.getElementById('info')
 function active() {
   const Ch = new XMLHttpRequest();
   const url='https://swapi.co/api/people/1';
-  Ch.open("GET", url);
-  Ch.send();
+
 
   Ch.onreadystatechange = function() {
     if (Ch.readyState === 4 && Ch.status === 200) {
       let datos = JSON.parse(Ch.responseText);
-      for (let i in datos) {
-        resultado.innerHTML += i + ": " + datos[i] + "<br>"
-      }
+      resultado.innerHTML += "This my man " + datos.name + "<br>"
     }
   }
 
-  Ch.open("GET", url, true);
+  Ch.open("GET", url);
   Ch.send();
+
 }
