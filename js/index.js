9e50cbc71b54558fb165d4f5d2414452f778ce4a
alessandro <aless0117@outlook.es>
2020-03-30 17:23:58 -0400

    Push for form

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ let pContainer = document.getElementById('container-products')
 const url = 'https://pidetuchuche-backend.herokuapp.com';
 const req = new XMLHttpRequest();
 //GET ALL
-function active() {
+function getProducts() {
   req.open('GET', url + '/productos', true);
   req.onreadystatechange = function () {
     if(req.readyState === XMLHttpRequest.DONE) {
@@ -16,10 +16,10 @@ function active() {
         let products = JSON.parse(this.responseText);
         console.log(products);
         for (let i = 0; i < products.datos.length; i++) {
-          let p = document.createElement('p');
-          p.innerHTML = products.datos[i].nombre;
-          p.value = products.datos[i].nombre;
-          pContainer.appendChild(p);
+          let div = document.createElement('div')
+          div.classList.add("container-products");
+          div.innerHTML = '<div class="wrapper cards"> <div class="row"> <div class="product-info col"><div class="product-text"><h1>' + products.datos[i].nombre + '</h1><h2>' + products.datos[i].nombre + '</div></div></div></div>';
+          pContainer.appendChild(div);
 
         }
       }
@@ -27,11 +27,13 @@ function active() {
   };
   req.send();
 }
+
 //GET categories
 function getCategorias() {
   req.open('GET', url + '/categorias', true);
   req.onreadystatechange = function () {
     if(req.readyState === XMLHttpRequest.DONE) {
+      console.log('done');
       let status = req.status;
       if (status === 0 || (200 >= status && status < 400)) {
         let categories = JSON.parse(this.responseText);
@@ -50,7 +52,10 @@ function getCategorias() {
   req.send();
 }
 
-getCategorias();
+ window.onload = function() {
+  getCategorias();
+ };
+
 
 function printTok() {
   let token = localStorage.getItem('authToken')
@@ -65,32 +70,37 @@ function createProduct() {
   let tokenCapitalized = token.charAt(0).toUpperCase() + token.substring(1);
   let product_name = document.getElementById('product_name').value;
   let product_img = document.getElementById('product_img');
-  let img_file = product_img.files[0];
   let product_description = document.getElementById('product_description').value;
   let product_quantity = document.getElementById('product_quantity').value;
   let product_priority = document.getElementById('product_priority').value;
   let product_price = document.getElementById('product_price').value;
   let product_discount = document.getElementById('product_discount').value;
+
   let product_category = document.getElementById('product_category').value;
-  let product_category1 = document.getElementById('product_category1').value;
 
 
   const productData = new FormData();
 
+  productData.append("ruta_imagen", product_img.files[0]);
   productData.append("nombre", product_name);
   productData.append("descripcion", product_description);
   productData.append("cantidad", product_quantity);
   productData.append("prioridad", product_priority);
   productData.append("precio", product_price);
   productData.append("descuento", product_discount);
-  // productData.append("categorias[0]", product_category);
-  // productData.append("categorias[1]", product_category1);
+  productData.append("categorias", product_category);
+
+  let producto = {};
+  productData.forEach((value, key) => {producto[key] = value});
+  let productoJSON = JSON.stringify(producto);
 
   req.open("POST", url + '/productos', true);
+
   req.setRequestHeader("Content-type", "application/json");
+
   req.setRequestHeader("Authorization", tokenCapitalized);
-  console.log(productData);
-  req.send(productData);
+  console.log(productoJSON);
+  req.send(productoJSON);
 
 }
 
@@ -125,3 +135,7 @@ function loginOnClick() {
     req.send(data);
   })
 }
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
